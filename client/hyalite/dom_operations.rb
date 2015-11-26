7ca5303fc9ff94b69b748e1b98b5d4ffc62055d6
youchan <youchan01@gmail.com>
2015-11-26 19:08:12 +0900

    Refactor class << self

--- client/hyalite/dom_operations.rb
+++ client/hyalite/dom_operations.rb
@@ -2,14 +2,15 @@ require 'hyalite/dom_property_operations'
 
 module Hyalite
   module DOMOperations
+    class << self
+      def update_property_by_id(id, name, value)
+        node = Mount.node(id)
 
-    def self.update_property_by_id(id, name, value)
-      node = Mount.node(id)
-
-      if value
-        DOMPropertyOperations.set_value_for_property(node, name, value)
-      else
-        DOMPropertyOperations.delete_value_for_property(node, name)
+        if value
+          DOMPropertyOperations.set_value_for_property(node, name, value)
+        else
+          DOMPropertyOperations.delete_value_for_property(node, name)
+        end
       end
     end
   end
