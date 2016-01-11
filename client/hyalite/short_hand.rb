1e2e260465bd02b0abbfe9a2eeefd1d2341a1183
youchan <youchan01@gmail.com>
2016-01-11 11:09:19 +0900

    Complete short hands
    i.e http://www.w3.org/TR/html-markup/elements.html

--- client/hyalite/short_hand.rb
+++ client/hyalite/short_hand.rb
@@ -1,7 +1,13 @@
 module Hyalite
   module Component
     module ShortHand
-      TAGS = %w(h1 h2 h3 h4 h5 h6 header footer section div p span code strong a img input button label ul li)
+      TAGS = %w(
+        a abbr address area article aside audio b base bdi bdo blockquote body br button button button button canvas caption
+        cite code col colgroup command datalist dd del details dfn div dl dt em embed fieldset figcaption figure footer form
+        h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link map mark menu meta
+        meter nav noscript object ol optgroup option output p param pre progress q rp rt ruby s samp script section select small
+        source span strong style sub summary sup table tbody td textarea tfoot th thead time title tr track u ul var video wbr
+      )
 
       def self.included(klass)
         TAGS.each do |tag|
