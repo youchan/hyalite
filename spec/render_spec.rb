5ae7ab039aa65e16fab9a70d6e86e50735f5b142
youchan <youchan01@gmail.com>
2016-03-01 22:06:13 +0900

    Refactor RenderingHelper

--- spec/render_spec.rb
+++ spec/render_spec.rb
@@ -1,18 +1,12 @@
 require 'spec_helper'
 
 describe 'render' do
+  include RenderingHelper
+
   it 'render plain div element' do
-    mount_at = DOM("<div class='root'></div>")
-    mount_at.append_to($document.body)
-
-    class TestComponent
-      include Hyalite::Component
-      def render
-        Hyalite.create_element('div', {className: 'actual'})
-      end
+    render do
+      Hyalite.create_element('div', {className: 'actual'})
     end
-    Hyalite.render(Hyalite.create_element(TestComponent), mount_at)
-
     expect($document['.actual']).to be_a(Browser::DOM::Element)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,21 @@
 require 'hyalite'
+
+module RenderingHelper
+  def self.included(mod)
+    mod.before do
+      @mount_at = DOM("<div class='root'></div>")
+      @mount_at.append_to($document.body)
+    end
+  end
+
+  def render(&block)
+    class TestComponent
+      include Hyalite::Component
+
+      define_method :render, &block
+    end
+
+    Hyalite.render(Hyalite.create_element(TestComponent), @mount_at)
+  end
+end
+
