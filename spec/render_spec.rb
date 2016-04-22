e2aeb390708da5760283ef55dcead5c5bb511f60
youchan <youchan01@gmail.com>
2016-04-22 13:27:31 +0900

    Test rendering child element

--- spec/render_spec.rb
+++ spec/render_spec.rb
@@ -9,4 +9,15 @@ describe 'render' do
     end
     expect($document['.actual']).to be_a(Browser::DOM::Element)
   end
+
+  it 'render child element' do
+    render do
+      Hyalite.create_element('div', {className: 'parent'}, Hyalite.create_element('div', {className: 'child'}))
+    end
+
+    parent = $document['.parent']
+    expect(parent).to be_a(Browser::DOM::Element)
+    expect(parent.child).to be_a(Browser::DOM::Element)
+    expect(parent.child.class_name).to be('child')
+  end
 end
