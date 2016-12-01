6f4e22662a630e39026779de311d1a1d79b154e5
youchan <youchan01@gmail.com>
2016-12-01 14:12:39 +0900

    Fix el method not work

--- client/hyalite/component.rb
+++ client/hyalite/component.rb
@@ -2,7 +2,17 @@ require 'hyalite/short_hand'
 
 module Hyalite
   module Component
-    include ShortHand
+    TAGS = %w(
+      a abbr address area article aside audio b base bdi bdo blockquote body br button button button button canvas caption
+      cite code col colgroup command datalist dd del details dfn div dl dt em embed fieldset figcaption figure footer form
+      h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link map mark menu meta
+      meter nav noscript object ol optgroup option output p param pre progress q rp rt ruby s samp script section select small
+      source span strong style sub summary sup table tbody td textarea tfoot th thead time title tr track u ul var video wbr
+    )
+
+    def pp(obj)
+      puts obj.inspect
+    end
 
     attr_accessor :props, :context, :refs
 
@@ -14,13 +24,27 @@ module Hyalite
       @refs = nil
     end
 
-    def self.included(clazz)
-      clazz.instance_eval do
+    def self.included(klass)
+      klass.instance_eval do
         define_singleton_method(:state) do |key, initial_value|
           (@initial_state ||= {})[key] = initial_value
         end
 
         define_singleton_method(:initial_state) { @initial_state || {} }
+      end
+
+      TAGS.each do |tag|
+        define_method(tag) do |props, *children|
+          Hyalite.create_element(tag, props, *children)
+        end
+      end
+
+      klass.extend ClassMethods
+    end
+
+    module ClassMethods
+      def el(props, *children)
+        Hyalite.create_element(self, props, *children)
       end
     end
 
--- client/hyalite/short_hand.rb
+++ client/hyalite/short_hand.rb
@@ -1,33 +1,6 @@
 module Hyalite
   module Component
     module ShortHand
-      TAGS = %w(
-        a abbr address area article aside audio b base bdi bdo blockquote body br button button button button canvas caption
-        cite code col colgroup command datalist dd del details dfn div dl dt em embed fieldset figcaption figure footer form
-        h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link map mark menu meta
-        meter nav noscript object ol optgroup option output p param pre progress q rp rt ruby s samp script section select small
-        source span strong style sub summary sup table tbody td textarea tfoot th thead time title tr track u ul var video wbr
-      )
-
-      def self.included(klass)
-        TAGS.each do |tag|
-          define_method(tag) do |props, *children|
-            Hyalite.create_element(tag, props, *children)
-          end
-        end
-
-        klass.extend ClassMethods
-      end
-
-      module ClassMethods
-        def el(props, *children)
-          Hyalite.create_element(self, props, *children)
-        end
-      end
-
-      def pp(obj)
-        puts obj.inspect
-      end
     end
   end
 end
